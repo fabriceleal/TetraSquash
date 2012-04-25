0464ea7818a695f7cb85c989f4e1bf904e09977d
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2012-04-25 15:49:17 +0100

    lots of progress. players slide in one axis. Ground behind the players so the ball bounces around. Movement of top and bottom players.

--- world_index4.js
+++ world_index4.js
@@ -28,6 +28,47 @@ function PhysicsWorld (intervalRate, adaptive, ctx, withDebug){
 	
 	this.world = new b2World(new b2Vec2(0, 0) /* gravity*/, true /*allow sleep*/);
 	
+	var sound = new Audio("ball.mp3");
+	
+	this.world.SetContactListener(
+		{			
+			BeginContact : function(contact){
+				//console.log('BeginContact');
+			},
+			EndContact : function(contact){
+				//console.log('EndContact');
+			},
+			PreSolve : function(contact, oldManifold){
+				if(contact.IsTouching()){
+					var bodya = contact.GetFixtureA().GetBody().GetUserData();
+					var bodyb = contact.GetFixtureB().GetBody().GetUserData();
+				
+					/*console.log(
+						contact.GetFixtureA().GetBody().GetUserData() + ' vs. ' +
+						contact.GetFixtureB().GetBody().GetUserData());
+					*/
+					var other = bodyb;
+					if(bodyb == 'ball'){
+						other = bodya;
+					}
+					else if(bodya != 'ball'){
+						return; /* neither body is a ball*/
+					}
+					
+					if(other.indexOf("ground") == 0){
+						other = other.replace("ground", "");
+						
+						var elem = document.getElementById(other);
+						elem.innerText = new Number(elem.innerText) + 1;
+					}
+				}
+			},
+			PostSolve : function(contact, impulse){
+				//console.log('PostSolve');
+			}		
+		}
+	);
+	
 	this.SCALE = 30;
 	
 	this.fixDef = new b2FixtureDef;
@@ -95,6 +136,16 @@ PhysicsWorld.prototype.update = function(callback){
 	}
 	
 	callback(world);
+
+	/*
+	var i = 0;
+	for(var x = this.world.GetContactList(); x != null; x = x.GetNext() ){
+		if(x.IsTouching()){
+			console.log(
+				x.GetFixtureA().GetBody().GetUserData() + ' vs. ' +
+				x.GetFixtureB().GetBody().GetUserData());
+		}
+	}*/
 }
 
 /*
@@ -104,14 +155,20 @@ PhysicsWorld.prototype.setBodies = function(bodyEntities){
 		
 	var entity = bodyEntities.ball;
 	
+	// Create ball
 	this.fixDef.shape = new b2CircleShape(entity.radius / this.SCALE);
-		
+	this.fixDef.density = 0.3;
+	this.fixDef.restitution = 1.0;
+	
 	this.bodyDef.type = b2Body.b2_dynamicBody;
+
 	this.bodyDef.position.x = entity.x / this.SCALE;
 	this.bodyDef.position.y = entity.y / this.SCALE;
 	this.bodyDef.userData = entity.id;
 	this.bodiesMap[entity.id] = this.world.CreateBody(this.bodyDef) 
 	this.bodiesMap[entity.id].CreateFixture(this.fixDef);
+
+	// * Ball created *
 
 	for(var i in bodyEntities.players){
 		entity = bodyEntities.players[i];
@@ -122,12 +179,56 @@ PhysicsWorld.prototype.setBodies = function(bodyEntities){
 		this.bodyDef.position.x = (entity.x + entity.width / 2) / this.SCALE;
 		this.bodyDef.position.y = (entity.y + entity.height / 2) / this.SCALE;
 		this.bodyDef.userData = entity.id;
+		this.bodyDef.linearDamping = 1.2;
 		
+		/* To make sure that the player can move the player
+		before the ball touches it !!! */
+		this.bodyDef.allowSleep = false;
+		this.bodyDef.awake = true;
+
 		this.fixDef.shape = new b2PolygonShape;
 		this.fixDef.shape.SetAsBox( (entity.width / 2) / this.SCALE, (entity.height / 2) / this.SCALE);
+		this.fixDef.density = 1.0;
+		this.fixDef.restitution = 0.2;
 		
 		this.bodiesMap[entity.id] = this.world.CreateBody(this.bodyDef) 
 		this.bodiesMap[entity.id].CreateFixture(this.fixDef);
+		
+		// Create ground (to joint with)
+		//if (entity.ground && entity.ground.x > 0){
+		this.bodyDef.type = b2Body.b2_staticBody;
+		this.bodyDef.position.x = (entity.ground.x ) / this.SCALE;
+		this.bodyDef.position.y = (entity.ground.y) / this.SCALE;
+		this.bodyDef.userData = 'ground' + entity.id;
+		this.fixDef.shape = new b2PolygonShape;
+		this.fixDef.shape.SetAsBox( (entity.ground.width / 2) / this.SCALE, (entity.ground.height / 2) / this.SCALE);
+				
+		this.bodiesMap['ground' + entity.id] = this.world.CreateBody(this.bodyDef);
+		this.bodiesMap['ground' + entity.id].CreateFixture(this.fixDef);
+		// * Ground created*
+
+		// Ugly hack
+		var axis = new b2Vec2(0.0, 1.0); // y axis
+		if(entity.ground.width > entity.ground.height){
+			// Horizontal ground
+			axis = new b2Vec2(1.0, 0.0) // x axis
+		}
+		
+		// create joint
+		var joint = new b2PrismaticJointDef();
+		joint.Initialize(
+					this.bodiesMap['ground' + entity.id], 
+					this.bodiesMap[entity.id], 
+					this.bodiesMap[entity.id].GetWorldCenter(),
+					axis
+					);
+
+		joint.lowerTranslation = 0.0;
+		joint.upperTranslation = 0.0;
+		
+		this.world.CreateJoint(joint);
+		// * Joint created *
+		//}/**/
 	}
 	
 	for(var i in bodyEntities.barriers){
@@ -145,14 +246,14 @@ PhysicsWorld.prototype.setBodies = function(bodyEntities){
 		this.bodiesMap[entity.id] = this.world.CreateBody(this.bodyDef) 
 		this.bodiesMap[entity.id].CreateFixture(this.fixDef);
 	}
-	
+	/*	
 	var joint = new b2PrismaticJointDef();
-	/*joint.Initialize(
+joint.Initialize(
 			this.bodiesMap['playerleft'], 
 			this.bodiesMap['playerbottom'], 
 			this.bodiesMap['playerbottom'].GetWorldCenter(),
 			new b2Vec2(1.0, 0.0));
-	*/
+
 	
 	joint.bodyA = this.bodiesMap['playerleft'];
 	joint.bodyB = this.bodiesMap['playerbottom'];
@@ -172,7 +273,7 @@ PhysicsWorld.prototype.setBodies = function(bodyEntities){
 	joint.enableMotor = true; 
 	joint.maxMotorForce = 0; 
 	joint.motorSpeed = 15;
-	
+		*/
 	/*joint.bodyA = this.bodiesMap['barrier_bottomleft']
 	joint.bodyB = this.bodiesMap['pbottom']
 	joint.collideConnected = false;
@@ -217,11 +318,21 @@ PhysicsWorld.prototype.setBodies = function(bodyEntities){
 	this.ready = true;
 }
 
-PhysicsWorld.prototype.applyImpulse = function(bodyId, power){
+PhysicsWorld.prototype.applyImpulse = function(bodyId, power, angle){
     var body = this.bodiesMap[bodyId];
-    body.ApplyImpulse(new b2Vec2(Math.cos(0 * (Math.PI / 180)) * power,
-                                 Math.sin(0 * (Math.PI / 180)) * power),
+    body.ApplyImpulse(new b2Vec2(Math.cos(angle*(Math.PI / 180)) * power,
+                                 Math.sin(angle*(Math.PI / 180)) * power),
                                  body.GetWorldCenter());
+}
+
+PhysicsWorld.prototype.setVelocity = function(bodyId, power, angle){
+    var body = this.bodiesMap[bodyId];
+    body.SetLinearVelocity(new b2Vec2(Math.cos(angle*(Math.PI / 180)) * power,
+                                 Math.sin(angle*(Math.PI / 180)) * power));
+	/*document.getElementById('dbg').innerHTML = 
+		'<p>' + 
+			(function(v){return v.x + ", " + v.y})(body.GetLinearVelocity()) + 
+		'</p>';*/
 }
 
 //var phWorld = new PhysicsWorld(30, false);
